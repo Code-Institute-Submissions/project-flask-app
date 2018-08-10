73a649bcf4caaed8f2b7789f9001141e977c3916
SDV <steindevos@gmail.com>
2018-08-10 10:07:34 +0000

    added a basic form and routing for the 'addtopic'

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 import os
 from flask import Flask, render_template, request, url_for, redirect
-
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 
+app.config["MONGO_DBNAME"] = "flask-app"
+app.config["MONGO_URI"] = "mongodb://admin:pa55word10@ds119072.mlab.com:19072/flask-app"
+mongo = PyMongo(app)
+
 @app.route("/")
 def show_index():
-    return "hello world"
+    return render_template("index.html")
+
+@app.route("/add")
+def addtopic():
+    return render_template("addtopic.html")
 
 
 
