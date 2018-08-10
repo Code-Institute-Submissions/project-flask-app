67393e119898dc290c575a87b945d8647442d8e3
SDV <steindevos@gmail.com>
2018-08-10 08:00:17 +0000

    returned a 'hello world' with Flask

---
+++ app.py
@@ -0,0 +1,17 @@
+import os
+from flask import Flask, render_template, request, url_for, redirect
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def show_index():
+    return "hello world"
+
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+    port=int(os.environ.get("PORT")),
+    debug=True
+    )
