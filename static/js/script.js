beb476baab5fe26a1c4834680b9258d762f5d017
SDV <steindevos@gmail.com>
2018-08-13 12:16:47 +0000

    Added a functionality to create flexible number of categories per topic

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,11 @@
-$(document).ready(function(){
+$(document).ready(function() {
     $('.sidenav').sidenav();
-  });
+
+    countCategory=0
+    $('#btnAddTopicCategory').click(function() {
+        let newDiv = $('<div class="row"><div class="input-field col s12"><input placeholder="text" id="new_category" type="text" class="validate" name="category'+countCategory+'"><label for="new_category">Category</label></div></div>');
+        $('#formWrapper').append(newDiv);
+        countCategory+=1
+    });
+
+});
