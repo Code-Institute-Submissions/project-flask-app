b6f974421fc1097efa9918b1990a709e67504446
SDV <steindevos@gmail.com>
2018-08-17 12:29:47 +0000

    Added new fields for the add project form

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,25 @@
 $(document).ready(function() {
     $('.sidenav').sidenav();
 
-    countCategory=0
-    $('#btnAddprojectCategory').click(function() {
-        let newDiv = $('<div class="input-field col s12"><input placeholder="text" id="new_category" type="text" class="validate" name="category'+countCategory+'"><label for="new_category">Category</label></div>');
-        $('#formWrapper').append(newDiv);
-        countCategory+=1
+    countCategoryText=0
+    countCategoryNumber=0
+    countCategoryBoolean=0
+    $('#btnAddprojectCategoryText').click(function() {
+        let newDivText = $('<div class="input-field col s12"><input placeholder="text" maxlength="40" id="new_category" type="text" class="validate" name="textcategory'+countCategoryText+'"><label for="new_category">Category title (text)</label></div>');
+        $('#formWrapper').append(newDivText);
+        countCategoryText+=1
+    });
+    
+    $('#btnAddprojectCategoryNumber').click(function() {
+        let newDivNumber = $('<div class="input-field col s12"><input maxlength="40" placeholder="text" id="new_category" type="text" class="validate" name="numbercategory'+countCategoryNumber+'"><label for="new_category">Category title (number)</label></div>');
+        $('#formWrapper').append(newDivNumber);
+        countCategoryNumber+=1
+    });
+    
+    $('#btnAddprojectCategoryBoolean').click(function() {
+        let newDivBoolean = $('<div class="input-field col s12"><input maxlength="40" placeholder="text" id="new_category" type="text" class="validate" name="booleancategory'+countCategoryBoolean+'"><label for="new_category">Category title (boolean)</label></div>');
+        $('#formWrapper').append(newDivBoolean);
+        countCategoryBoolean+=1
     });
     
     $('.btnRemoveprojectCategory').click(function() {
