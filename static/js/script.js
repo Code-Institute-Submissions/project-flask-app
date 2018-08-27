a7e7f9e0d07e7bd1dc20166cd3cfcf26145de594
SDV <steindevos@gmail.com>
2018-08-27 20:41:11 +0000

    added functionaliyt for adding photos and videos to a case

--- static/js/script.js
+++ static/js/script.js
@@ -1,28 +1,46 @@
 $(document).ready(function() {
     $('.sidenav').sidenav();
 
-    countCategoryText=0
-    countCategoryNumber=0
-    countCategoryBoolean=0
+    countCategoryText = 0;
+    countCategoryNumber = 0;
+    countCategoryBoolean = 0;
+    countCasePicture = 0;
+    countCaseVideo = 0;
+
+
     $('#btnAddprojectCategoryText').click(function() {
-        let newDivText = $('<div class="input-field col s12"><input placeholder="text" maxlength="40" id="new_category" type="text" class="validate" name="textcategory'+countCategoryText+'"><label for="new_category">Category title (text)</label></div>');
+        let newDivText = $('<div class="input-field col s12"><input placeholder="text" maxlength="40" id="new_category" type="text" class="validate" name="textcategory' + countCategoryText + '"><label for="new_category">Category title (text)</label></div>');
         $('#formWrapper').append(newDivText);
-        countCategoryText+=1
+        countCategoryText += 1
     });
-    
+
     $('#btnAddprojectCategoryNumber').click(function() {
-        let newDivNumber = $('<div class="input-field col s12"><input maxlength="40" placeholder="text" id="new_category" type="text" class="validate" name="numbercategory'+countCategoryNumber+'"><label for="new_category">Category title (number)</label></div>');
+        let newDivNumber = $('<div class="input-field col s12"><input maxlength="40" placeholder="text" id="new_category" type="text" class="validate" name="numbercategory' + countCategoryNumber + '"><label for="new_category">Category title (number)</label></div>');
         $('#formWrapper').append(newDivNumber);
-        countCategoryNumber+=1
+        countCategoryNumber += 1
     });
-    
+
     $('#btnAddprojectCategoryBoolean').click(function() {
-        let newDivBoolean = $('<div class="input-field col s12"><input maxlength="40" placeholder="text" id="new_category" type="text" class="validate" name="booleancategory'+countCategoryBoolean+'"><label for="new_category">Category title (boolean)</label></div>');
+        let newDivBoolean = $('<div class="input-field col s12"><input maxlength="40" placeholder="text" id="new_category" type="text" class="validate" name="booleancategory' + countCategoryBoolean + '"><label for="new_category">Category title (boolean)</label></div>');
         $('#formWrapper').append(newDivBoolean);
-        countCategoryBoolean+=1
+        countCategoryBoolean += 1
     });
-    
+
     $('.btnRemoveprojectCategory').click(function() {
         $(this).closest('div').remove()
-    })
+    });
+    $('#btnAddcasePicture').click(function() {
+        let newDivPicture = $('<div class="input-field col s12"><input placeholder="image URL should start with' + " 'http' " + '(Get nice images from pexels.com :D )" id="new_picture" type="text" class="validate" name="image' + countCasePicture + '"><label for="new_picture">Add picture url</label></div>');
+        $('#caseForm').append(newDivPicture);
+        countCasePicture += 1
+    });
+    $('#btnAddcaseVideo').click(function() {
+        let newDivVideo = $('<div class="input-field col s12"><input placeholder="video URL should start with' + " 'http' " + '(Get nice videos from Youtube ... duhh! :P )" id="new_video" type="text" class="validate" name="video' + countCaseVideo + '"><label for="new_video">Add video url</label></div>');
+        $('#caseForm').append(newDivVideo);
+        countCaseVideo += 1
+    });
+
+    $('.carousel.carousel-slider').carousel({
+        fullWidth: true
+    });
 });
